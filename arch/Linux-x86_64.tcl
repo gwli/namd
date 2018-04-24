da71d7bb2735119b51d761e3d1fc5c872e552f78
victor <vili@nvidia.com>
2018-04-24 15:11:15 +0800

    fix cuda and tcl path issue

--- arch/Linux-x86_64.tcl
+++ arch/Linux-x86_64.tcl
@@ -1,9 +1,10 @@
 
 #TCLDIR=/Projects/namd2/tcl/tcl8.5.9-linux-x86_64
-TCLDIR=/Projects/namd2/tcl/tcl8.5.9-linux-x86_64-threaded
-TCLINCL=-I$(TCLDIR)/include
+TCLDIR=/usr/include/tcl8.6
+TCLINCL=-I$(TCLDIR)
 #TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl -lpthread
+#TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl
+TCLLIB=-ltcl8.6 -ldl -lpthread
 TCLFLAGS=-DNAMD_TCL
 TCL=$(TCLINCL) $(TCLFLAGS)
 
---
+++ devtools/build.sh
@@ -0,0 +1,19 @@
+TOP=`pwd`/..
+
+#CHARM=$TOP/charm
+#
+#
+#cd $CHARM
+#./build charm++ multicore-linux-x86_64 --with-production
+##./build charm++ mpi-linux-x86_64 --with-production
+#
+#make -j
+#
+#apt update && apt install -y libfftw3-dev libfftw3-mpi-dev tcl8.6 tcl8.6-dev
+#
+#
+cd $TOP
+rm -fr Linux-x86_64-g++
+./config Linux-x86_64-g++ --charm-arch multicore-linux-x86_64 --with-cuda
+cd $TOP/Linux-x86_64-g++
+make 
